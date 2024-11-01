fe2f34932e0be00d72151a8cc7a5b00d29029b8f
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2024-11-01 21:47:57 +0200

    Adding read & write fns

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Test comment
+def read_file(filename):
+    """Reads the content of a file and returns it as a string."""
+    with open(filename, 'r') as file:
+        data = file.read()
+    return data
+
+def write_file(filename, data):
+    """Writes data to a file."""
+    with open(filename, 'w') as file:
+        file.write(data)
